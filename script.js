912bdf332bd25e382bd20a22e4d80f93661e1658
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-05-14 09:49:10 -0700

    add some styling to it

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let count = 1;
 document.addEventListener("DOMContentLoaded", function () {
   var list = document.getElementById("list");
   var add_task = document.getElementById("add_task");
@@ -6,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   submit_btn.onclick = () => {
     var div = document.createElement("div");
+    div.className = "list_item";
     var li = document.createElement("li");
     li.textContent = add_task.value;
     var delete_btn = document.createElement("button");
 
     delete_btn.type = "button";
     delete_btn.textContent = "delete";
-    delete_btn.id = "delete-" + count;
     delete_btn.addEventListener("click", () => {
       div.remove();
     });
@@ -21,6 +20,5 @@ document.addEventListener("DOMContentLoaded", function () {
     div.appendChild(delete_btn);
     list.appendChild(div);
     add_task.value = "";
-    count++;
   };
 });
