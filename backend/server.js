7a3f2f3f7a0bbc52e4d6f406dfa728567c7c03e1
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-05-19 16:35:37 -0700

    redirect html

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://192.168.1.219:5500",
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 const users = [];
+
+app.get("/", (req, res) => {
+  //res.redirect("http://192.168.1.219:5500");
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
+
 app.get("/users", (req, res) => {
   res.json(users);
 });
 
-app.post("/login", (req, res) => {
-  const user = { name: req.body.name, password: req.body.password };
+app.post("/register", (req, res) => {
+  const user = { username: req.body.name, password: req.body.password };
   users.push(user);
   res.json(req.body);
 });
