653e6f5e566f28a78a44ea535158e5b463d8c95f
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-05-18 17:51:10 -0700

    install neccessary dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
   console.log(`Server is listening to port ${port}`);
 });
