746987cfc89983a76c4317544455d6c78c5ab3b5
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-05-18 17:53:51 -0700

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,15 @@ const bcrypt = require("bcrypt");
 const app = express();
 const port = 3000;
 
+// middleware and routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/to-do", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(port, () => {
   console.log(`Server is listening to port ${port}`);
