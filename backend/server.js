a8c7ba6900068ad000e859c4dfc2d8173a16d5e7
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-05-21 13:37:03 -0700

    setup .env and connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
-const userRoute = require("./routes/User");
+const userRoute = require("./routes/UserRoute");
+const User = require("./User");
 const app = express();
 const port = 3000;
+
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://192.168.1.219:5500",
+    origin: process.env.FRONTEND_URL,
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE", "PATCH"],
     credentials: true,
   })
 );
 
+//Connect MongoDB
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//console.log(process.env.MONGODB_URL);
+
 app.get("/", (req, res) => {
-  //res.redirect("http://192.168.1.219:5500");
   res.sendFile(path.join(__dirname, "./public", "index.html"));
 });
 
