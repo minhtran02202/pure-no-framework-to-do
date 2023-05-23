cd9a8ca7b1b4115ba442160ede56b8da0dcbc907
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-05-22 22:34:21 -0700

    put index.html to frontend folder

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const userRoute = require("./routes/UserRoute");
-const User = require("./User");
 const app = express();
 const port = 3000;
 
 require("dotenv").config();
-
-app.use(express.json());
++app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
@@ -20,18 +18,30 @@ app.use(
 );
 
 //Connect MongoDB
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log(`Connected to ${result.connection.name}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 //console.log(process.env.MONGODB_URL);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public", "index.html"));
-});
+/*app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});*/
 
 app.use("/user", userRoute);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+});
+
+app.use(express.static("../frontend"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
